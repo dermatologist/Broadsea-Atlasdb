8846a3965694c9a1e140c272ffe3984fb42656fd
dermatologist <github@gulfdoctor.net>
2025-08-18 06:18:42 -0500

    Update schema path script to use lowercase syntax and comment out old search path

--- 150_create_schema_path.sql
+++ 150_create_schema_path.sql
@@ -1,3 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS my_cdm;
 
-SET SEARCH PATH TO my_cdm,demo_cdm,demo_cdm_results,"$user",public;
+-- SET SEARCH PATH TO my_cdm,demo_cdm,demo_cdm_results,"$user",public;
+
+set search_path = my_cdm,demo_cdm,demo_cdm_results,"$user",public;
